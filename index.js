6e5519d2957e787f1b64bd11ca7f899272358b77
sebagalindo <cmsgalindo@icloud.com>
2020-03-26 18:41:21 -0300

    done

--- index.js
+++ index.js
@@ -21,7 +21,42 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const recipe1 = new Recipe({
+      title: "Pie",
+      level: "Easy Peasy",
+      ingredients: ["flour", "egg", "milk", "cocoa powder", "crystal sugar"],
+      cuisine: "American",
+      dishType: "dessert",
+      duration: 40,
+      creator: "God"
+    });
+    Recipe.create(recipe1)
+      .then(() => {
+        Recipe.insertMany(data)
+          .then(() => {
+            Recipe.findOneAndUpdate({ title: /gatoni/i }, { duration: 100 })
+              .then(() => {
+                console.log("Duration changed!");
+                Recipe.deleteOne({ title: /arrot/ })
+                  .then(() => {
+                    console.log("Carrot Cake deleted");
+                    Recipe.find()
+                      .then(res => {
+                        res.forEach(item => {
+                          console.log(item.title);
+                        });
+                        mongoose.connection.close();
+                      })
+                      .catch(error => console.log(error));
+                  })
+                  .catch(error => console.log(error));
+              })
+              .catch(error => console.log(error));
+          })
+          .catch(error => console.log(error));
+      })
+      .catch(error => console.log(error));
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    console.error("Error connecting to the database", error);
   });
